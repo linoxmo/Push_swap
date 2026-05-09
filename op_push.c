e4a9852a6e443675ed3f14e1d2bdd3d5645e2984
Ralph <ralph8@live.fr>
2026-05-09 20:40:18 +0200

    feat: push added

---
+++ op_push.c
@@ -0,0 +1,47 @@
+#include "push_swap.h"
+
+void	push(t_list **from, t_list **to)
+{
+	t_list	*tmp;
+
+	if (!from || !to || !*from)
+		return ;
+	tmp = (*from)->next;
+	(*from)->next = *to;
+	*to = *from;
+	*from = tmp;
+}
+
+void	pa(t_list **a, t_list **b)
+{
+	push(b, a);
+}
+
+void	pb(t_list **a, t_list **b)
+{
+	push(a, b);
+}
+
+int main(void)
+{
+	t_list	*a;
+	t_list	*b;
+	int		i;
+	int		len;
+
+	len = 5;
+	i = 0;
+	while (i < len)
+		ft_lstadd_back(&a, ft_lstnew(i++));
+	while (i < len + 5)
+		ft_lstadd_back(&b, ft_lstnew(i++));
+	printf("the stacks before\n");
+	ft_lstprint(a);
+	printf("\n");
+	ft_lstprint(b);
+	pa(&a, &b);
+	printf("the stacks after\n");
+	ft_lstprint(a);
+	printf("\n");
+	ft_lstprint(b);
+}
---
+++ op_swap.c
@@ -0,0 +1,43 @@
+#include "push_swap.h"
+
+void	swap(t_list **lst)
+{
+	t_list	*node;
+
+	if (!lst || !*lst || !(*lst)->next)
+		return ;
+	node = (*lst)->next;
+	(*lst)->next = node->next;
+	node->next = (*lst);
+	(*lst) = node;
+}
+
+void	sa(t_list **a)
+{
+	swap(a);
+}
+
+void	sb(t_list **b)
+{
+	swap(b);
+}
+
+void	ss(t_list **a, t_list **b)
+{
+	swap(a);
+	swap(b);
+}
+
+// int main(void)
+// {
+// 	t_list	*a;
+// 	int		i;
+// 	int		len;
+//
+// 	len = 5;
+// 	i = 0;
+// 	while (i < len)
+// 		ft_lstadd_back(&a, ft_lstnew(i++));
+// 	sa(&a);
+// 	ft_lstprint(a);
+// }
