d13ffbfc72c5f83562cf8ae0a35ebcad7bf631bb
Ralph <ralph8@live.fr>
2026-05-10 15:54:46 +0200

    updated op

--- op_reverse.c
+++ op_reverse.c
@@ -32,26 +32,26 @@ void	rrr(t_list **a, t_list **b)
 	reverse_rotate(b);
 }
 
-int main(void)
-{
-	t_list	*a;
-	t_list	*b;
-	int		i;
-	int		len;
-
-	len = 5;
-	i = 0;
-	while (i < len)
-		ft_lstadd_back(&a, ft_lstnew(i++));
-	while (i < len + 5)
-		ft_lstadd_back(&b, ft_lstnew(i++));
-	printf("the stacks before\n");
-	ft_lstprint(a);
-	printf("\n");
-	ft_lstprint(b);
-	rrr(&a, &b);
-	printf("the stacks after\n");
-	ft_lstprint(a);
-	printf("\n");
-	ft_lstprint(b);
-}
+// int main(void)
+// {
+// 	t_list	*a;
+// 	t_list	*b;
+// 	int		i;
+// 	int		len;
+//
+// 	len = 5;
+// 	i = 0;
+// 	while (i < len)
+// 		ft_lstadd_back(&a, ft_lstnew(i++));
+// 	while (i < len + 5)
+// 		ft_lstadd_back(&b, ft_lstnew(i++));
+// 	printf("the stacks before\n");
+// 	ft_lstprint(a);
+// 	printf("\n");
+// 	ft_lstprint(b);
+// 	rrr(&a, &b);
+// 	printf("the stacks after\n");
+// 	ft_lstprint(a);
+// 	printf("\n");
+// 	ft_lstprint(b);
+// }
