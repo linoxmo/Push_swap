089073b0776984505dc5cbbdb49b0200141de2ce
Ralph <ralph8@live.fr>
2026-05-10 16:35:54 +0200

    feat: added op to .h file

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tmagoudi <ttmagoudi@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 17:39:53 by tmagoudi          #+#    #+#             */
-/*   Updated: 2026/05/10 16:29:10 by rfeghali         ###   ########.fr       */
+/*   Updated: 2026/05/10 16:35:32 by rfeghali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,23 @@ typedef struct	s_list
 	struct  s_list *next;
 }		t_list;
 
+/* stack operations */
+void	pa(t_list **a, t_list **b);
+void	pb(t_list **a, t_list **b);
+void	sa(t_list **a);
+void	sb(t_list **b);
+void	ss(t_list **a, t_list **b);
+void	ra(t_list **a);
+void	rb(t_list **b);
+void	rs(t_list **a, t_list **b);
+void	rra(t_list **a);
+void	rrb(t_list **b);
+void	rrs(t_list **a, t_list **b);
+
+/* list utils */
 void 	ft_lstadd_back(t_list **lst, t_list *new);
+
+/* general utils */
 int		ft_atoi(char *str);
 
 #endif
