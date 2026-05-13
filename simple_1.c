f1c309765d28592b4d68702e8d11aa94820454fc
tmagoudi <tmagoudi@f6r7s7.paris.42.school>
2026-05-13 17:59:14 +0200

    simple_1.c

--- simple_1.c
+++ simple_1.c
@@ -6,16 +6,37 @@
 /*   By: tmagoudi <tmagoudi@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 16:16:56 by tmagoudi          #+#    #+#             */
-/*   Updated: 2026/05/12 16:23:54 by tmagoudi         ###   ########.fr       */
+/*   Updated: 2026/05/13 14:19:02 by tmagoudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack* ft_simple_1(t_stack *a, t_stack*b)
-{
-	temp
+t_stack *ft_simple_1(t_stack *a, t_stack *b) {
+  int i;
+  int min;
+  int pos;
+  t_list *temp;
 
-
-	return (a);
+  i = 0;
+  min = a->top->content;
+  pos = 0;
+  temp = a->top;
+  while (temp) {
+    while (i < a->size) {
+      temp = temp->next;
+      if (temp->content <= min) {
+        min = temp->content;
+        pos = i;
+      }
+      i++;
+    }
+    i = 0;
+    while (i < pos) {
+      ra(a);
+      i++;
+    }
+    pb(a, b);
+  }
+  return (a);
 }
